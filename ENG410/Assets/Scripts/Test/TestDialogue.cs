ca248438a600f9b6cd374fc90aec48965cb40f3f
grammss <44184597+grammss@users.noreply.github.com>
2021-10-22 23:50:19 +0800

    Update

---
+++ ENG410/Assets/Scripts/Test/TestDialogue.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDialogue : MonoBehaviour
+{
+  public string[] s =
+  {
+    "Hello, I am Grammama",
+    "I like big butts and I cannot lie",
+    "LULW",
+  };
+
+  int index = 0;
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+      if (!DialogueSystem.inst.isSpeaking
+        || DialogueSystem.inst.isWaitingForUserInput)
+      {
+        if (index >= s.Length)
+        {
+          return;
+        }
+        Say(s[index]);
+        ++index;
+      }
+    }
+  }
+
+  void Say(string s)
+  {
+    string[] parts = s.Split(':');
+    string speech = parts[0];
+    string speaker = (parts.Length > 1) ? parts[1] : "";
+
+    DialogueSystem.inst.Say(speech, speaker);
+  }
+}
